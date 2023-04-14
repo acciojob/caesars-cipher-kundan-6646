32086faee6d0321b8fdc82dece90ffc1a796c26b
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-04-14 23:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+   let decodedArr = ''; // Your Result goes here
   for (let ch of encodedStr) {
-  	decodedArr.push(lookup[ch]);
+  	decodedArr += lookup[ch];
   }
 	
   return decodedArr;
@@ -21,6 +21,6 @@ function rot13(encodedStr){
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+alert(rot13("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
 
 module.exports = rot13;
